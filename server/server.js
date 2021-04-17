d77557e82a82f3791e3a2ef13accc148aaca8663
Cecilia Zhang <czhang3@caltech.edu>
2021-04-17 16:37:59 -0700

    Data now in simple database

--- server/server.js
+++ server/server.js
@@ -4,8 +4,9 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const port = 8000;
-const jwt = require("express-jwt"); // NEW
-const jwksRsa = require("jwks-rsa"); // NEW
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+var db = require("./database.js");
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -17,7 +18,6 @@ const authConfig = {
   audience: "https://quandary-api"
 };
 
-// NEW
 // Create middleware to validate the JWT using express-jwt
 const checkJwt = jwt({
   // Provide a signing key based on the key identifier in the header and the signing keys provided by your Auth0 JWKS endpoint.
@@ -34,47 +34,30 @@ const checkJwt = jwt({
     algorithms: ["RS256"]
   });
 
-let events =
-[
-  {
-    id: 1,
-    name: 'Charity Ball',
-    category: 'Fundraising',
-    description: 'Spend an elegant night of dinner and dancing with us as we raise money for our new rescue farm.',
-    featuredImage: 'https://placekitten.com/500/500',
-    images: [
-      'https://placekitten.com/500/500',
-      'https://placekitten.com/500/500',
-      'https://placekitten.com/500/500',
-    ],
-    location: '1234 Fancy Ave',
-    date: '12-25-2019',
-    time: '11:30'
-  },
-  {
-    id: 2,
-    name: 'Rescue Center Goods Drive',
-    category: 'Adoptions',
-    description: 'Come to our donation drive to help us replenish our stock of pet food, toys, bedding, etc. We will have live bands, games, food trucks, and much more.',
-    featuredImage: 'https://placekitten.com/500/500',
-    images: [
-      'https://placekitten.com/500/500'
-    ],
-    location: '1234 Dog Alley',
-    date: '11-21-2019',
-    time: '12:00'
-  }
-];
 
 app.get('/quandary', (req, res) => {
-  res.send(events);
+    var sql = "select * from quandary"
+      var params = []
+      db.all(sql, params, (err, rows) => {
+          if (err) {
+            res.status(400).json({"error":err.message});
+            return;
+          }
+          res.send(rows)
+        });
 });
 
 // For this app, we only want to protect the route that returns the details of an event
 app.get('/quandary/:id', checkJwt, (req, res) => {
-  const id = Number(req.params.id);
-  const event = events.find(event => event.id === id);
-  res.send(event);
+  var sql = "select * from quandary where id = ?"
+    var params = [req.params.id]
+    db.get(sql, params, (err, row) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json(row)
+      });
 });
 
 app.get('/', (req, res) => {
