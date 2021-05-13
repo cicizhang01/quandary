d21ddef0806c7a840d1529b77aa5d24413815789
Cecilia Zhang <czhang3@caltech.edu>
2021-05-12 21:41:50 -0700

    Forgot a file

---
+++ quandary-app/src/views/Forum.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="about">
+    <div class="hero is-light">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title is-size-3">Ask a Question</h1>
+        </div>
+      </div>
+    </div>
+    <QuestionsList />
+  </div>
+</template>
+<script>
+import QuestionsList from '../components/QuestionsList';
+export default {
+  name: 'home',
+  components: {
+    QuestionsList
+  },
+  methods: {
+    // Log the user in
+    login() {
+      this.$auth.loginWithRedirect();
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .org-description {
+    margin-top: 50px;
+  }
+</style>
