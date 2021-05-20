e4e24892477d910179f4436592ce602f5730e6fc
Cecilia Zhang <czhang3@caltech.edu>
2021-05-20 16:33:56 -0700

    Adjusted spacing

--- quandary-app/src/components/QuestionsList.vue
+++ quandary-app/src/components/QuestionsList.vue
@@ -1,44 +1,48 @@
 <template>
   <div class="events container">
     <div class="columns is-multiline">
-        <div v-for="question in questions" :question="question" :key="question.question_id" class="column is-full">
-          <div class="question-card">
-                <div class="card">
-                    <div class="columns">
-                        <div class="column is-1" id="upvotes">
-                            <div>
-                                <button class="button is-white is-medium" v-if="user.upvotes.includes(question.question_id)" v-on:click="onUpdateQuestionCount(question)">
-                                    <span class="icon is-small">
-                                        <i class="fas fa-heart" id="solid-heart"></i>
-                                    </span>
-                                </button>
-                                <button class="button is-white is-medium" v-else v-on:click="onUpdateQuestionCount(question)">
-                                    <span class="icon is-small">
-                                        <i class="far fa-heart" id="regular-heart"></i>
-                                    </span>
-                                </button>
-                                <div class="upvotes-text">{{question.question_upvotes}}</div>
-                            </div>
-                        </div>
-                        
-                        <div class= "column is-11">
-                            <h2 class="question-date">{{ displayDate(question.date_modified) }}</h2>
-                            <div class = "test">
-                                <router-link :to="`/quandary/${question.question_id}`">
-                                    <h1 class="title">{{ question.question_body }}</h1>
-                                </router-link>
-                            </div>
-                            <span class="tag is-primary is-medium" id="question-topic" v-for="topic in topics" v-bind:key="topic">
-                                {{ topic }}
-                            </span>
-                        </div>
-                    </div>
+      <div v-for="question in questions" :question="question" :key="question.question_id" class="column is-full">
+        <div class="question-card">
+          <div class="card">
+            <div class="columns">
+              <div class="column is-1" id="upvotes">
+                <div>
+                  <button class="button is-white is-medium" v-if="user.upvotes.includes(question.question_id)" v-on:click="onUpdateQuestionCount(question)">
+                    <span class="icon is-small">
+                      <i class="fas fa-heart" id="solid-heart"></i>
+                    </span>
+                  </button>
+
+                  <button class="button is-white is-medium" v-else v-on:click="onUpdateQuestionCount(question)">
+                    <span class="icon is-small">
+                      <i class="far fa-heart" id="regular-heart"></i>
+                    </span>
+                  </button>
+                  
+                  <div class="upvotes-text">{{question.question_upvotes}}</div>
                 </div>
+              </div>
+                        
+              <div class= "column is-11">
+                <h2 class="question-date">{{ displayDate(question.date_modified) }}</h2>
+                  <div class = "test">
+                    <router-link :to="`/quandary/${question.question_id}`">
+                      <h1 class="title">{{ question.question_body }}</h1>
+                    </router-link>
+                  </div>
+
+                  <span class="tag is-primary is-medium" id="question-topic" v-for="topic in topics" v-bind:key="topic">
+                    {{ topic }}
+                  </span>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
     </div>
   </div>
 </template>
+
 <script>
 import QuandaryService from '@/services/QuandaryService.js';
 
@@ -64,68 +68,64 @@ export default {
     };
   }, 
   created() {
-      this.getData();
+    this.getData();
   },
   methods : {
-        async getData() {
-            QuandaryService.getQuestionsData()
-                .then(
-                    (questions => {
-                        this.$set(this, "questions", questions);
-                    }).bind(this)
-                );
-        },
-
-        displayDate(currDate) {
-            var datetime = currDate.split(" ");
-            var date = datetime[0].split("-");
-            var time = datetime[1].split(":");
-            var month = date[1];
-            var day = date[2];
-            var year = date[0];
-            var hour = time[0];
-            var minutes = time[1];
-      
-            return month + "/" + day + "/" + year + " " + hour + ":" + minutes;
-        },
-
-        getDateTime() {
-            var time = new Date();
-            var month = ('0' + (time.getMonth() + 1)).slice(-2);
-            var date = ('0' + time.getDate()).slice(-2);
-            var year = time.getFullYear();
-            var hour = time.getHours();
-            var minutes = time.getMinutes();
-            var seconds = time.getSeconds();
-            
-            return year + "-" + month + "-" + date + " " + hour + ":" + minutes + ":" + seconds;
-        },
-
-        onUpdateQuestionCount(question) {
-            const index = this.user.upvotes.indexOf(question.question_id);
-            if (index > -1) {
-                this.user.upvotes.splice(index, 1);
-                question.question_upvotes -= 1;
-            }
-            else {
-                this.user.upvotes.push(question.question_id);
-                question.question_upvotes += 1;
-            }
-        },
-
+    async getData() {
+      QuandaryService.getQuestionsData()
+        .then(
+          (questions => {
+            this.$set(this, "questions", questions);
+          }).bind(this)
+        );
     },
-        
+
+    displayDate(currDate) {
+      var datetime = currDate.split(" ");
+      var date = datetime[0].split("-");
+      var time = datetime[1].split(":");
+      var month = date[1];
+      var day = date[2];
+      var year = date[0];
+      var hour = time[0];
+      var minutes = time[1];
+      
+      return month + "/" + day + "/" + year + " " + hour + ":" + minutes;
+    },
+
+    getDateTime() {
+      var time = new Date();
+      var month = ('0' + (time.getMonth() + 1)).slice(-2);
+      var date = ('0' + time.getDate()).slice(-2);
+      var year = time.getFullYear();
+      var hour = time.getHours();
+      var minutes = time.getMinutes();
+      var seconds = time.getSeconds();
+            
+      return year + "-" + month + "-" + date + " " + hour + ":" + minutes + ":" + seconds;
+    },
+
+    onUpdateQuestionCount(question) {
+      const index = this.user.upvotes.indexOf(question.question_id);
+      if (index > -1) {
+        this.user.upvotes.splice(index, 1);
+        question.question_upvotes -= 1;
+      }
+      else {
+        this.user.upvotes.push(question.question_id);
+        question.question_upvotes += 1;
+      }
+    },
+  },      
 };
 </script>
+
 <style lang="scss" scoped>
 @import "../assets/colors";
 .questions {
   margin-top: 50px;
   text-align: left;
 }
-
-
-
 .card {
   text-align: left;
   background-color: rgba(128, 128, 128, 0.021);
@@ -133,11 +133,11 @@ export default {
   border-radius: 1rem;
   box-shadow: 0 .5rem 1rem rgba(0,0,0,.15);
   .question-date {
-      font-size: 1rem;
-      color: gray;
+    font-size: 1rem;
+    color: gray;
   }
   .title {
-      margin: 0.5rem 0 1rem 0;
+    margin: 0.5rem 0 1rem 0;
   }
 }
 .question-date {
@@ -147,20 +147,20 @@ export default {
   right: 0;
 }
 #question-topic {
-    margin: -1rem 0.75rem 0 0;
+  margin: -1rem 0.75rem 0 0;
 }
 #upvotes {
-    display: flex;
-    text-align: center;
-    padding: 1rem 0.5rem 0.5rem;
-    .upvotes-text {
-      color: $light-gray;
-    }
-    #solid-heart {
-      color: $pink;
-    }
-    #regular-heart {
-      color: $light-gray;
-    }
+  display: flex;
+  text-align: center;
+  padding: 1rem 0.5rem 0.5rem;
+  .upvotes-text {
+    color: $light-gray;
   }
+  #solid-heart {
+    color: $pink;
+  }
+  #regular-heart {
+    color: $light-gray;
+  }
+}
 </style>
--- quandary-app/src/views/Forum.vue
+++ quandary-app/src/views/Forum.vue
@@ -1,58 +1,71 @@
 <template>
   <div class="about">
     <div class="events container">
-        <div>
-            <button class="button is-primary" v-on:click="isHidden = !isHidden">Ask a question!</button>
-        </div>
-        <h1 v-if="!isHidden">
+      <div>
+          <button class="button is-primary" v-on:click="isHidden = !isHidden">Ask a question!</button>
+      </div>
+
+      <h1 v-if="!isHidden">
         <div class="comment">
-            <div class="columns">
-                <div class="column is-11">
-                    <div class="comment-label is-grouped">
-                        Ask as
-                        <div class="select" id="comment-username">
-                            <select>
-                                <option value=0>{{ this.$auth.user.nickname }}</option> <!-- Replace with current user's first and last name; initial selected value has to match v-model-->
-                                <option value=1>Anonymous</option>
-                            </select>
-                        </div>
-                    </div>
+          <div class="columns">
+
+            <div class="column is-11">
+              <div class="comment-label is-grouped">
+                Ask as
+                <div class="select" id="comment-username">
+                  <select>
+                    <option value=0> {{ this.$auth.user.nickname }} </option>
+                    <option value=1>Anonymous</option>
+                  </select>
                 </div>
-                <div class="column is-1">
-                    <button class="button is-pulled-right is-primary" v-on:click="onSubmit()">Submit</button>
-                </div>
+              </div>
             </div>
-        
-        <div class="question-box has-addons">
-                <textarea class="textarea has-fixed-size" placeholder="Ask your question"></textarea>
+
+            <div class="column is-1">
+              <button class="button is-pulled-right is-primary" v-on:click="onSubmit()">Submit</button>
+            </div>
+
+          </div>
+
+          <div class="question-box has-addons">
+                  <textarea class="textarea has-fixed-size" placeholder="Ask your question"></textarea>
+          </div>
         </div>
-        </div>
-        </h1>
+      </h1>
+
     </div>
     <QuestionsList />
   </div>
 </template>
+
 <script>
 import QuestionsList from '../components/QuestionsList';
+
 export default {
   name: 'home',
+
   components: {
     QuestionsList
   },
+
   data() {
-        return {
-            isHidden: true
-        }
-    },
+    return {
+      isHidden: true
+    }
+  },
+
   methods: {
     // Log the user in
     login() {
       this.$auth.loginWithRedirect();
-    },
+    }
   }
+
 }
+
 </script>
 <style lang="scss" scoped>
+@import "../assets/colors";
   .org-description {
     margin-top: 50px;
   }
@@ -60,18 +73,18 @@ export default {
     margin-top: 1rem;
   }
   .comment {
-      padding: 1.25rem 0;
-      padding-bottom: 0.4rem;
-      .comment-label {
-        // color: $light-gray;
-        display: flex;
-        align-items: center;
-        #comment-username {
-          margin-left: 0.75rem;
-        }
-      }
-      .question-box {
-        margin-top: 1rem;
+    padding: 1.25rem 0;
+    padding-bottom: 0.4rem;
+    .comment-label {
+      color: $light-gray;
+      display: flex;
+      align-items: center;
+      #comment-username {
+        margin-left: 0.75rem;
       }
     }
+    .question-box {
+      margin-top: 1rem;
+    }
+  }
 </style>
