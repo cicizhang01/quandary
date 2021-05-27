e3a3b4feb81ec8d1b07c0de8dad3090f2d589eb9
Meena H <meenarh414@gmail.com>
2021-05-27 11:21:04 -0700

    update valid helper

--- quandary-app/src/components/ValidationHelper.vue
+++ quandary-app/src/components/ValidationHelper.vue
@@ -19,20 +19,20 @@ export default {
     },
     computed:{
         rules() {
-            if(this.validationRules)
-                return this.validationRules[this.storeState.currentTab] ? this.validationRules[this.storeState.currentTab] : {}
+            if(this.validation_rules)
+                return this.validation_rules[this.storeState.currentTab] ? this.validation_rules[this.storeState.currentTab] : {}
             else
                 return {};
         },
     },
     methods:{
         hasError(fieldName){
-            return (fieldName in this.$v.formData) && (this.$v.formData[fieldName].$error)
+            return (fieldName in this.$v.form_data) && (this.$v.form_data[fieldName].$error)
         }
     },
     validations() {
         return {
-            formData : this.rules
+            form_data : this.rules
         };
     }
 }
